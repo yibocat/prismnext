442e8a2b9b35e7c9fdd58e67f7b5d65ecb92627c
Yibo <yibocat@yeah.net>
2026-05-21 02:17:27 +0800

    feat: wrap App component in GlobalErrorBoundary and load settings on mount

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,5 +1,8 @@
+import { useEffect } from "react";
 import { ThemeProvider } from "next-themes";
 import { useLayoutStore } from "@/stores/layout-store";
+import { useSettingsStore } from "@/stores/settings-store";
+import { GlobalErrorBoundary } from "@/components/global-error-boundary";
 import { TitleBar } from "@/components/layout/title-bar";
 import { LeftSidebar } from "@/components/layout/left-sidebar";
 import { LeftMainArea } from "@/components/layout/left-main-area";
@@ -9,20 +12,27 @@ import { BottomBar } from "@/components/layout/bottom-bar";
 export function App() {
   const rightAreaExpanded = useLayoutStore((s) => s.rightAreaExpanded);
   const editorMaximized = useLayoutStore((s) => s.editorMaximized);
+  const loadSettings = useSettingsStore((s) => s.loadSettings);
+
+  useEffect(() => {
+    loadSettings();
+  }, [loadSettings]);
 
   return (
-    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-      <div className="flex h-full flex-col">
-        <TitleBar />
+    <GlobalErrorBoundary>
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <div className="flex h-full flex-col">
+          <TitleBar />
 
-        <div className="flex flex-1 min-h-0 overflow-hidden">
-          <LeftSidebar />
-          {!editorMaximized && <LeftMainArea />}
-          {(rightAreaExpanded || editorMaximized) && <RightArea maximized={editorMaximized} />}
+          <div className="flex flex-1 min-h-0 overflow-hidden">
+            <LeftSidebar />
+            {!editorMaximized && <LeftMainArea />}
+            {(rightAreaExpanded || editorMaximized) && <RightArea maximized={editorMaximized} />}
+          </div>
+
+          <BottomBar />
         </div>
-
-        <BottomBar />
-      </div>
-    </ThemeProvider>
+      </ThemeProvider>
+    </GlobalErrorBoundary>
   );
 }
